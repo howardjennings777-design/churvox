ad96dee1e2ffd11417c4a7c4867985ed60c30eca
howardjennings77-ui <howardjennings77@gmail.com>
2026-04-04 21:26:20 +0000

    Revert "Force create first job to emergency page and clear cache"
    This reverts commit be0d38911fed2bbbc23c249a29ed41a59858571e.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,12 +16,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.getRegistrations().then((regs) => {
-      regs.forEach((reg) => reg.unregister());
-    });
-  });
-}
