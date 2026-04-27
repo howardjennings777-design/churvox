ea87d9419c2ae2ab69716742819364a94f5362ab
howardjennings777-design <howardjennings777@gmail.com>
2026-04-27 22:46:55 +1200

    Load compact dispatch board styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import "./styles/churvox-unified-headers.css";
 import "./styles/churvox-team-area-groups.css";
 import "./styles/churvox-client-area-groups.css";
 import "./styles/churvox-jobs-compact.css";
+import "./styles/churvox-dispatch-compact.css";
 import { startTeamTownGroupingEnhancer } from "./utils/teamTownGroupingEnhancer";
 import { startClientAreaGroupingEnhancer } from "./utils/clientAreaGroupingEnhancer";
 import { startAutomationActionFixer } from "./utils/automationActionFixer";
