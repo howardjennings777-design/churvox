82f6df320caf9450d689321b16e0d9ada76f159f
howardjennings777-design <howardjennings777@gmail.com>
2026-05-03 15:59:02 +1200

    Use existing sonner toast in Smart Hub

--- frontend/src/pages/SmartHubBrainPage.js
+++ frontend/src/pages/SmartHubBrainPage.js
@@ -4,7 +4,7 @@ import { AlertTriangle, Bot, CalendarClock, CheckCircle2, ClipboardList, FileTex
 import Layout from "../components/Layout";
 import { get, post } from "../lib/api";
 import { useAuth } from "../context/AuthContext";
-import { toast } from "react-hot-toast";
+import { toast } from "sonner";
 import JobCreateForm from "../components/forms/JobCreateForm";
 import QuoteCreateForm from "../components/forms/QuoteCreateForm";
 import InvoiceCreateForm from "../components/forms/InvoiceCreateForm";
