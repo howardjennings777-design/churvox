8ab60dd64381f1eaafdfcd36862565167fe19914
howardjennings777-design <howardjennings777@gmail.com>
2026-05-05 09:36:18 +1200

    Switch dashboard to wired control room

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,12 +1,8 @@
 import React from "react";
-import AIControlRoomPage from "./AIControlRoomPage";
+import AIControlRoomWiredPage from "./AIControlRoomWiredPage";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
 import "../styles/aiControlRoomForceV4.css";
 
 export default function DashboardPage() {
-  return (
-    <SmartHubErrorBoundary>
-      <AIControlRoomPage />
-    </SmartHubErrorBoundary>
-  );
+  return <SmartHubErrorBoundary><AIControlRoomWiredPage /></SmartHubErrorBoundary>;
 }
