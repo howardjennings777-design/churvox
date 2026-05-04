4cbb3293f4ca34f57dd880cdbbd9ac859edbb068
howardjennings777-design <howardjennings777@gmail.com>
2026-05-05 09:07:10 +1200

    Import visible AI Control Room V4 override

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AIControlRoomPage from "./AIControlRoomPage";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
+import "../styles/aiControlRoomForceV4.css";
 
 export default function DashboardPage() {
   return (
