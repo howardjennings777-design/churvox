01b39288060a599d111dd229672300b31eeed4f6
howardjennings777-design <howardjennings777@gmail.com>
2026-05-05 06:55:50 +1200

    Apply modern AI Control Room polish

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import CommandHubTopPlayerPage from "./CommandHubTopPlayerPage";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
 import "../styles/smartHubStableFix.css";
+import "../styles/commandHubModernFix.css";
 
 export default function DashboardPage() {
   return <SmartHubErrorBoundary><CommandHubTopPlayerPage /></SmartHubErrorBoundary>;
