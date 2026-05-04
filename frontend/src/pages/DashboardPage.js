549cc835838ecca0b29ade3814f059bf19480277
howardjennings777-design <howardjennings777@gmail.com>
2026-05-04 20:01:28 +1200

    Load Smart Hub stable polish override

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SmartHubBrainPageStable from "./SmartHubBrainPageStable";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
+import "../styles/smartHubStableFix.css";
 
 export default function DashboardPage() {
   return <SmartHubErrorBoundary><SmartHubBrainPageStable /></SmartHubErrorBoundary>;
