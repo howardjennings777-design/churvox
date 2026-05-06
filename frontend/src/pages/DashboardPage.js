dcf83710effce4e51efb8ac9d4928a4c585af9e6
howardjennings777-design <howardjennings777@gmail.com>
2026-05-07 05:41:19 +1200

    Use editable action-based Smart Hub dashboard

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import AIControlRoomLaunchPage from "./AIControlRoomLaunchPage";
+import AIControlRoomActionPage from "./AIControlRoomActionPage";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
 import "../styles/aiControlRoomForceV4.css";
 
 export default function DashboardPage() {
   return (
     <SmartHubErrorBoundary>
-      <AIControlRoomLaunchPage />
+      <AIControlRoomActionPage />
     </SmartHubErrorBoundary>
   );
 }
