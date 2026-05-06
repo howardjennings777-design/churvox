f8ab5143317cf09907fe6bcb4aca074e5c4d5960
howardjennings777-design <howardjennings777@gmail.com>
2026-05-07 01:55:51 +1200

    Use functional AI Control Room launch dashboard

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import AIControlRoomCompletePage from "./AIControlRoomCompletePage";
+import AIControlRoomLaunchPage from "./AIControlRoomLaunchPage";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
 import "../styles/aiControlRoomForceV4.css";
 
 export default function DashboardPage() {
   return (
     <SmartHubErrorBoundary>
-      <AIControlRoomCompletePage />
+      <AIControlRoomLaunchPage />
     </SmartHubErrorBoundary>
   );
 }
