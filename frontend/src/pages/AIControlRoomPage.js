8d099ea00013133be1ae1700a479abbc4a140463
howardjennings777-design <howardjennings777@gmail.com>
2026-05-07 07:43:17 +1200

    Switch AI Operator to clean top-player Smart Hub

--- frontend/src/pages/AIControlRoomPage.js
+++ frontend/src/pages/AIControlRoomPage.js
@@ -1,5 +1,5 @@
-import SmartHubExactPage from "./SmartHubExactPage";
+import SmartHubTopPlayerFinal from "./SmartHubTopPlayerFinal";
 
 export default function AIControlRoomPage() {
-  return <SmartHubExactPage />;
+  return <SmartHubTopPlayerFinal />;
 }
