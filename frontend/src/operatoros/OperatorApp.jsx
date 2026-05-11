69378e017f5013b8f2f3ac1a006cb2e64fd082e2
howardjennings777-design <howardjennings777@gmail.com>
2026-05-11 23:19:42 +0000

    Apply final Operator OS sidebar logo fix

--- frontend/src/operatoros/OperatorApp.jsx
+++ frontend/src/operatoros/OperatorApp.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import "./operatorTheme.css";
 import "./operatorDesignFinal.css";
 import "./operatorLaptopPolish.css";
+import "./operatorSidebarFinal.css";
 import FreshChurvoxApp from "../fresh/FreshChurvoxApp";
 import OperatorShell from "./OperatorShell";
 import { useOperatorData } from "./dataHooks";
