728df929bf58c658e91e579a77e05cf323d14672
howardjennings777-design <howardjennings777@gmail.com>
2026-05-13 23:50:27 +0000

    Remove old global Operator CSS mixing with new layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -33,6 +33,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./operatoros/operatorImageStyle.css";
 async function clearOldPwaShell() {
   if (typeof window === "undefined") return;
 
