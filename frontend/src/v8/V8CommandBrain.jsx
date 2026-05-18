2261ccfc7ae52676c7a8d19160174412357676b0
howardjennings777-design <howardjennings777@gmail.com>
2026-05-18 23:10:56 +0000

    Revert "Fix V8CommandBrain import for restored app routing"
    This reverts commit 8a1af456d59036e3cb48bc83853263b1b4d74752.

--- frontend/src/v8/V8CommandBrain.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import OperatorMachine from "../operator-machine/OperatorMachine";
-
-export default function V8CommandBrain(props) {
-  return <OperatorMachine {...props} />;
-}
