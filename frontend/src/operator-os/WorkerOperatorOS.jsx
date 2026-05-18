5312da35447855121aab7fdd8ec24f19460b4a2c
howardjennings777-design <howardjennings777@gmail.com>
2026-05-18 22:54:13 +0000

    Fix WorkerOperatorOS import for restored app routing

---
+++ frontend/src/operator-os/WorkerOperatorOS.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import WorkerCockpitPage from "../pages/worker/WorkerCockpitPage";
+
+export default function WorkerOperatorOS(props) {
+  return <WorkerCockpitPage {...props} />;
+}
