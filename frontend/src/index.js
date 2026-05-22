59caa2ab93b95fe1913506ffdd99cdd0595ceb5d
howardjennings777-design <howardjennings777@gmail.com>
2026-05-22 21:55:13 +1200

    Remove failed SteelWorks CSS import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './styles/churvoxFullPageModals.css';
-import './styles/churvox-steelworks-pages.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
