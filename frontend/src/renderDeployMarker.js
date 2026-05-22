d9b42d97df8588c68ef7798c75cc2d57c8d86457
howardjennings777-design <howardjennings777@gmail.com>
2026-05-23 01:17:58 +1200

    Add Render deploy marker

---
+++ frontend/src/renderDeployMarker.js
@@ -0,0 +1,3 @@
+// RENDER_DEPLOY_FORCE_MARKER_20260522_WORKHORSE
+// This file is imported by index.js so Render produces a fresh JS bundle on deploy.
+export const renderDeployMarker = 'RENDER_DEPLOY_FORCE_MARKER_20260522_WORKHORSE';
