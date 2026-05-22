a4189934d1c569648e8bba2a3da654d9fe12831b
howardjennings777-design <howardjennings777@gmail.com>
2026-05-22 16:42:31 +1200

    Use Churvox Machine theme imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./styles/premium.css";
 import "./styles/churvoxFullPageModals.css";
-import "./styles/churvox-executive-master.css";
-import "./styles/churvox-autonomous-home.css";
-import "./styles/churvox-autonomous-features.css";
-import "./styles/churvox-autonomous-pricing.css";
-import "./styles/churvox-autonomous-auth.css";
-import "./styles/churvox-autonomous-office-desk.css";
-import "./styles/churvox-autonomous-workspaces.css";
-import "./styles/churvox-autonomous-chrome.css";
-import "./styles/churvox-autonomous-final-sweep.css";
-import "./pages/marketing/AutonomousHomePage.css";
+import "./styles/churvox-machine.css";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
