a3054bf97125c6ebc05ca9723d750772afdef60b
howardjennings777-design <howardjennings777@gmail.com>
2026-05-23 22:52:19 +0000

    Add deep operations theme across app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import './styles/churvox-workhorse-ops.css';
 import './styles/aiDashPolish.css';
 import './styles/churvoxControlSurfaceApp.css';
 import './styles/churvoxForceControlSurface.css';
+import './styles/churvoxDeepOperationsTheme.css';
 import './workhorseLiveCheck';
 import './renderDeployMarker';
 import './deploy/guidedOperatorFloorDeployMarker';
