af41159450975583de1689e56d86220da81c4a35
howardjennings777-design <howardjennings777@gmail.com>
2026-05-23 22:01:45 +0000

    Build Churvox Command Table workspace

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import SmartHubErrorBoundary from "../components/SmartHubErrorBoundary";
-import CommandDeskWorkspace from "./CommandDeskWorkspace";
+import CommandTableWorkspace from "./CommandTableWorkspace";
 
 export default function DashboardPage() {
   return (
     <SmartHubErrorBoundary>
-      <CommandDeskWorkspace />
+      <CommandTableWorkspace />
     </SmartHubErrorBoundary>
   );
 }
