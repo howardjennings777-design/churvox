c48d9f422c0adf33aacf07473cf9e639077b6a9d
howardjennings777-design <howardjennings777@gmail.com>
2026-05-24 08:42:42 +1200

    Load AI dashboard polish CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './styles/churvox-workhorse.css';
 import './styles/churvox-workhorse-worker.css';
 import './styles/churvox-workhorse-worker-complete.css';
 import './styles/churvox-workhorse-ops.css';
+import './styles/aiDashPolish.css';
 import './workhorseLiveCheck';
 import './renderDeployMarker';
 
