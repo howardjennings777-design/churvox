0be8a864b4cc5ba99815029433c6a50bea96afd4
howardjennings777-design <howardjennings777@gmail.com>
2026-05-27 07:38:54 +1200

    Load balanced Command Floor layout polish

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import './concept-c/conceptCCommandV34FinalPolish.css';
 import './concept-c/conceptCCommandV37RemoveHeroLogo.css';
 import './concept-c/conceptCCommandV38UsefulPolish.css';
 import './concept-c/commandFloorActionHubBoxes.css';
+import './concept-c/commandFloorBalancedLayout.css';
 import './workhorseLiveCheck';
 import './renderDeployMarker';
 import './deploy/guidedOperatorFloorDeployMarker';
