4bfd86989da90990b5ea6c0fd0979b81065b51f0
howardjennings777-design <howardjennings777@gmail.com>
2026-05-26 16:06:49 +1200

    Import Command Floor runtime markers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import './workhorseLiveCheck';
 import './renderDeployMarker';
 import './deploy/guidedOperatorFloorDeployMarker';
 import './deploy/commandFloorTestingDeployMarker';
+import './deploy/commandFloorRuntimeMarkers';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
