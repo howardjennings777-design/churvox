7ac0aa9a23ecf02af65738b37170a398c68e17e0
howardjennings777-design <howardjennings777@gmail.com>
2026-05-30 07:25:00 +1200

    Load Plans link in app navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,6 +26,7 @@ import './concept-c/churvoxTopTierRuntimePatch';
 import './concept-c/churvoxWorkerOfflineRuntimePatch';
 import './concept-c/churvoxActivePresetJobRuntimePatch';
 import './concept-c/churvoxLaunchNavRuntimePatch';
+import './concept-c/churvoxPlansNavPatch';
 import './concept-c/churvoxWorkSlipLinkedActionsBridge';
 import './concept-c/churvoxInvoiceJobContextPatch';
 import './concept-c/churvoxMoneyDeskLinkedJobFilterPatch';
