65b5241064ccf6d3cc24d8494743eda02d032159
howardjennings777-design <howardjennings777@gmail.com>
2026-05-31 11:40:47 +1200

    Import final Command Floor clarity CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,7 @@ import './concept-c/CommandRealPagesUsabilityFix.css';
 import './concept-c/CommandRealPagesBadgeFix.css';
 import './concept-c/CommandHideTopNav.css';
 import './concept-c/CommandBusinessPulseBigger.css';
+import './concept-c/CommandFloorClearActionFinal.css';
 import './pages/PlansFullScreenFix.css';
 import './pages/PlansSmsBuyPatch';
 import './concept-c/churvoxTopTierRuntimePatch';
