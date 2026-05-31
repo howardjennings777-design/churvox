853cafc76732594651d9c20b9cb18935e93c5274
howardjennings777-design <howardjennings777@gmail.com>
2026-06-01 10:14:20 +1200

    Restore job detail import wrapper

---
+++ frontend/src/pages/jobs/JobDetailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import JobDetailPageStable from "./JobDetailPageStable.jsx";
+
+function JobDetailPage(props) {
+  return <JobDetailPageStable {...props} />;
+}
+
+export default JobDetailPage;
