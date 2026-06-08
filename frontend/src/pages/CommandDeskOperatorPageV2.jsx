095840cd03f3d22957f5c8238eb9a8b4bb82afdc
howardjennings777-design <howardjennings777@gmail.com>
2026-06-09 10:59:40 +1200

    Remove floating setup overlay from Command

--- frontend/src/pages/CommandDeskOperatorPageV2.jsx
+++ frontend/src/pages/CommandDeskOperatorPageV2.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import CommandDeskOperatorPageV6 from "./CommandDeskOperatorPageV6";
-import FirstSetupGuide from "../components/FirstSetupGuide";
 
 function CommandDeskOperatorPageV2() {
-  return <><CommandDeskOperatorPageV6 /><FirstSetupGuide /></>;
+  return <CommandDeskOperatorPageV6 />;
 }
 
 export default CommandDeskOperatorPageV2;
