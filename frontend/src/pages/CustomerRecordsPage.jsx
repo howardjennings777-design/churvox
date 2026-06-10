05c438d09607ce779de6123d7643927721901404
howardjennings777-design <howardjennings777@gmail.com>
2026-06-10 16:39:38 +1200

    Wrap clients board in clean V2 shell

--- frontend/src/pages/CustomerRecordsPage.jsx
+++ frontend/src/pages/CustomerRecordsPage.jsx
@@ -1,5 +1,10 @@
+import V2Shell from "../churvox-v2/V2Shell";
 import ClientsV2Page from "../churvox-v2/ClientsV2Page";
 
 export default function CustomerRecordsPage() {
-  return <ClientsV2Page />;
+  return (
+    <V2Shell active="clients">
+      <ClientsV2Page />
+    </V2Shell>
+  );
 }
