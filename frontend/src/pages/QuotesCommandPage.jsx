5ab00f3b224ac71f73d3c9adc5526f63710c2612
howardjennings777-design <howardjennings777@gmail.com>
2026-06-10 16:45:27 +1200

    Switch quotes board to Churvox V2

--- frontend/src/pages/QuotesCommandPage.jsx
+++ frontend/src/pages/QuotesCommandPage.jsx
@@ -1,5 +1,5 @@
-import SafeRecordsWorkbenchPage from "./SafeRecordsWorkbenchPage";
+import WorkspaceV2Page from "../churvox-v2/WorkspaceV2Page";
 
 export default function QuotesCommandPage() {
-  return <SafeRecordsWorkbenchPage type="quotes" />;
+  return <WorkspaceV2Page area="quotes" />;
 }
