5b6c685ab9ffdb699b7eff459f1ab0fdb5efe694
howardjennings777-design <howardjennings777@gmail.com>
2026-06-12 11:15:52 +1200

    Load landing theme after base styles

--- frontend/src/pages/marketing/ExecutiveHomePage.jsx
+++ frontend/src/pages/marketing/ExecutiveHomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ChurvoxLogo } from "../../components/ChurvoxLogo";
 import "./SimplePublic.css";
+import "../../styles/public-dark-login-theme.css";
 
 const proof = ["14-day free trial", "No card to start", "AI setup guide", "Owner approves first"];
 
