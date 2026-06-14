ece69130b268312d48c301f3b5af4bc012b4a413
howardjennings777-design <howardjennings777@gmail.com>
2026-06-14 12:50:25 +1200

    Add new Churvox mark to public footer

--- frontend/src/pages/marketing/ExecutiveHomePage.jsx
+++ frontend/src/pages/marketing/ExecutiveHomePage.jsx
@@ -31,7 +31,10 @@ export function Nav() {
 export function Footer() {
   return (
     <footer className="simpleFooter">
-      <div><b>Churvox</b><span>Less admin. More jobs done.</span></div>
+      <div className="simpleFooterBrand">
+        <ChurvoxLogo variant="mark" size="md" />
+        <span><b>Churvox</b><small>Less admin. More jobs done.</small></span>
+      </div>
       <nav>
         <Link to="/pricing">Pricing</Link>
         <Link to="/features">How it works</Link>
