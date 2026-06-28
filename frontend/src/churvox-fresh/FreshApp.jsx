41a977444a866e85cb94704841d71a0457d69744
howardjennings777-design <howardjennings777@gmail.com>
2026-06-28 19:34:27 +1200

    Load real Clients workspace

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,4 +1,5 @@
 import "../runtime/churvoxTodayLanguageGuard";
+import "../runtime/churvoxClientsWorkspace";
 import "../churvox-os/churvoxOSRealAdminV2.css";
 import "../churvox-os/churvoxOSTodayFirst.css";
 import "../churvox-os/churvoxOSTodayOrganized.css";
@@ -6,6 +7,7 @@ import "../churvox-os/churvoxOSTodayTidy.css";
 import "../churvox-os/churvoxOSTodayEqualBoxes.css";
 import "../churvox-os/churvoxOSTodayFinalPolish.css";
 import "../churvox-os/churvoxOSJobsBoardFix.css";
+import "../churvox-os/churvoxOSClientsWorkspace.css";
 import ChurvoxOSRealAdminV3 from "../churvox-os/ChurvoxOSRealAdminV3";
 
 export default ChurvoxOSRealAdminV3;
