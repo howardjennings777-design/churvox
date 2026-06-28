1bca07df60d298fc04551e68544ac41ce254f673
howardjennings777-design <howardjennings777@gmail.com>
2026-06-28 18:59:58 +1200

    Load tidy Today sheet

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -2,6 +2,7 @@ import "../runtime/churvoxTodayLanguageGuard";
 import "../churvox-os/churvoxOSRealAdminV2.css";
 import "../churvox-os/churvoxOSTodayFirst.css";
 import "../churvox-os/churvoxOSTodayOrganized.css";
+import "../churvox-os/churvoxOSTodayTidy.css";
 import ChurvoxOSRealAdminV3 from "../churvox-os/ChurvoxOSRealAdminV3";
 
 export default ChurvoxOSRealAdminV3;
