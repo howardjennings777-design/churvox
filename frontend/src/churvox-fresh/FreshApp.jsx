f2d945c3278a91e12fc588702f524be7f0fd20f4
howardjennings777-design <howardjennings777@gmail.com>
2026-06-28 18:54:21 +1200

    Load organized Today view

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,5 +1,7 @@
+import "../runtime/churvoxTodayLanguageGuard";
 import "../churvox-os/churvoxOSRealAdminV2.css";
 import "../churvox-os/churvoxOSTodayFirst.css";
+import "../churvox-os/churvoxOSTodayOrganized.css";
 import ChurvoxOSRealAdminV3 from "../churvox-os/ChurvoxOSRealAdminV3";
 
 export default ChurvoxOSRealAdminV3;
