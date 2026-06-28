fec8953718da8e80e98c8e83a3eeb9c1dfb31e5b
howardjennings777-design <howardjennings777@gmail.com>
2026-06-28 16:56:43 +1200

    Ensure action polish overrides OS styles

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,4 +1,4 @@
-import "../churvox-os/churvoxOSActionFix.css";
 import ChurvoxOSProper from "../churvox-os/ChurvoxOSProper";
+import "../churvox-os/churvoxOSActionFix.css";
 
 export default ChurvoxOSProper;
