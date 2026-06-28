89ba55213e9fa0dfd5c5abe75d13ac07f3857509
howardjennings777-design <howardjennings777@gmail.com>
2026-06-28 19:07:24 +1200

    Load equal Today boxes

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -3,6 +3,7 @@ import "../churvox-os/churvoxOSRealAdminV2.css";
 import "../churvox-os/churvoxOSTodayFirst.css";
 import "../churvox-os/churvoxOSTodayOrganized.css";
 import "../churvox-os/churvoxOSTodayTidy.css";
+import "../churvox-os/churvoxOSTodayEqualBoxes.css";
 import ChurvoxOSRealAdminV3 from "../churvox-os/ChurvoxOSRealAdminV3";
 
 export default ChurvoxOSRealAdminV3;
