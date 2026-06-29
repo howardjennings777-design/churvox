4e1c9613448ae720a3a566097a94855322206653
howardjennings777-design <howardjennings777@gmail.com>
2026-06-29 15:55:23 +1200

    Load worker page split runtime

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,8 @@ import './components/recordWorkspacePopup.css';
 import './runtime/authInputVisibilityGuard';
 import './runtime/churvoxLaunchSplashRuntime';
 import './runtime/churvoxWorkerFieldFlowRuntime';
+import './runtime/churvoxWorkerPageSplitRuntime';
+import './runtime/churvoxWorkerPageSplitRuntime.css';
 import './pages/marketing/PublicAdminOS.css';
 import './runtime/churvoxPlansCountryRuntime';
 import './runtime/churvoxKiwiCopyGuard';
