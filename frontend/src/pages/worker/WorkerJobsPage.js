d6f5d92f0079afb425e65ce0341983520e0ef603
howardjennings777-design <howardjennings777@gmail.com>
2026-06-29 17:21:20 +1200

    Point worker list routes to simple worker app

--- frontend/src/pages/worker/WorkerJobsPage.js
+++ frontend/src/pages/worker/WorkerJobsPage.js
@@ -1,9 +1,1 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { WorkerTodayPage, WorkerJobsPage as RebuiltWorkerJobsPage } from "./WorkerRebuildPages";
-
-export default function WorkerJobsRoute() {
-  const location = useLocation();
-  if (location.pathname === "/worker/today" || location.pathname === "/worker") return <WorkerTodayPage />;
-  return <RebuiltWorkerJobsPage />;
-}
+export { default } from "./WorkerSimplePages";
