299437cb6faa6cf5392af317cebc0184b5806b3a
howardjennings777-design <howardjennings777@gmail.com>
2026-06-30 15:33:46 +1200

    Load Fresh drawer save runtime

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -52,6 +52,7 @@ import './churvox-fresh/optionFRecordHydrationRuntime';
 import './churvox-fresh/optionFProblemSlipRuntime';
 import './churvox-fresh/optionFAiFillMissingRuntime';
 import './churvox-fresh/optionFDrawerPersistenceRuntime';
+import './churvox-fresh/optionFDrawerSaveRuntime';
 import './churvox-fresh/optionFRealReviewFeatureRuntime.css';
 import './churvox-fresh/optionFRealReviewBackendSyncRuntime.css';
 import './churvox-fresh/optionFReadinessActionFixRuntime';
