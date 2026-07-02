36d5678369a15425eaa5be11c95f6d2a6525d734
howardjennings777-design <howardjennings777@gmail.com>
2026-07-02 19:18:40 +1200

    Import final proof rail hide override

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -79,6 +79,7 @@ import './styles/churvoxResponsiveFit.css';
 import './styles/churvoxPaidLaunchGuard.css';
 import './styles/churvoxPaidLaunchNoOverflowFinal.css';
 import './runtime/churvoxPaidLaunchOverflowRuntime';
+import './styles/churvoxHideProofRailFinal.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
