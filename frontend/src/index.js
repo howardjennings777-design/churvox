10131f9755193266bdd904c901f4061a4d11534e
howardjennings777-design <howardjennings777@gmail.com>
2026-07-02 20:35:54 +1200

    Apply modern Churvox polish stylesheet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './styles/churvoxFullPageModals.css';
 import './pages/marketing/PublicAdminOS.css';
+import './churvox-clean/ChurvoxModernPolish.css';
 import './runtime/authInputVisibilityGuard';
 
 const staticPublicPageRendered = Boolean(
