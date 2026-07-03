70006eb3b8dfc5e9bd95ac9d343e0f6061aa3dac
howardjennings777-design <howardjennings777@gmail.com>
2026-07-04 08:35:54 +1200

    Load top tier feature layer safely

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,6 +28,7 @@ import './runtime/churvoxPlansCountryRuntime';
 import './runtime/churvoxKiwiCopyGuard';
 import './runtime/churvoxPublicBusinessPagesRuntime';
 import './runtime/churvoxPublicBusinessPolishRuntime';
+import './runtime/churvoxTopTierFeaturesRuntime';
 import './pages/worker/WorkerNoFussPolish.css';
 import './churvox-fresh/optionFPageActionRuntime';
 import './churvox-fresh/optionFRestoreTimersRuntime';
