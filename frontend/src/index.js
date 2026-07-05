4045f01275ec56d6f95accd928318f3dcb106990
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 11:45:16 +1200

    Remove old modal CSS imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './styles/churvoxFullPageModals.css';
-import './components/recordWorkspacePopup.css';
 import './runtime/authInputVisibilityGuard';
 import './runtime/churvoxLaunchSplashRuntime';
 import './runtime/churvoxPlainSendGuardRuntime';
