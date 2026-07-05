e6658ec1de2dd02f8e742ab704d2fe49198ee9ab
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 11:28:15 +1200

    Trim frontend boot to clean Churvox app runtime

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import RecordWorkspacePopupBridgeV2 from './components/RecordWorkspacePopupBridgeV2';
 import './index.css';
 import './styles/churvoxFullPageModals.css';
 import './components/recordWorkspacePopup.css';
@@ -9,14 +8,9 @@ import './runtime/authInputVisibilityGuard';
 import './runtime/churvoxLaunchSplashRuntime';
 import './runtime/churvoxPlainSendGuardRuntime';
 import './runtime/churvoxLogoutNavRuntime';
-import './pages/marketing/PublicAdminOS.css';
 import './runtime/churvoxPlansCountryRuntime';
-import './runtime/churvoxPlansDuplicateShieldRuntime';
 import './runtime/churvoxPlansBillingNavClickGuard';
 import './runtime/churvoxKiwiCopyGuard';
-import './runtime/churvoxPublicBusinessPagesRuntime';
-import './runtime/churvoxPublicBusinessPolishRuntime';
-import './churvox-product/productModern.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -35,7 +29,6 @@ if (!staticPublicPageRendered) {
   root.render(
     <React.StrictMode>
       <App />
-      <RecordWorkspacePopupBridgeV2 />
     </React.StrictMode>
   );
 }
@@ -44,54 +37,7 @@ let ownerRuntimeLoaded = false;
 let workerRuntimeLoaded = false;
 
 const ownerRuntimeImports = [
-  () => import('./runtime/churvoxWorkerPreReactShell'),
-  () => import('./runtime/churvoxNativeTimerRuntime'),
-  () => import('./runtime/churvoxRouteAliasRuntime'),
-  () => import('./runtime/churvoxDrawerClickSafetyRuntime'),
   () => import('./runtime/churvoxPlanPersistenceRuntime'),
-  () => import('./runtime/churvoxFirstRunSetupRuntime'),
-  () => import('./runtime/churvoxCommandPreparedSlipsRuntime'),
-  () => import('./runtime/churvoxPlatformOwnerNoiseGuardRuntime'),
-  () => import('./churvox-fresh/optionFPageActionRuntime'),
-  () => import('./churvox-fresh/optionFRestoreTimersRuntime'),
-  () => import('./churvox-fresh/optionFControlPagesRuntime'),
-  () => import('./churvox-fresh/optionFControlPagesRuntimeGuard'),
-  () => import('./churvox-fresh/optionFPlansLiveCheckoutRuntimeV2'),
-  () => import('./churvox-fresh/optionFPlansIsolationRuntime'),
-  () => import('./churvox-fresh/optionFStripeManagedPriceDisplayRuntime'),
-  () => import('./churvox-fresh/optionFPlansUsageRuntime'),
-  () => import('./churvox-fresh/optionFBackendSyncRuntime'),
-  () => import('./churvox-fresh/optionFImportExportRuntime'),
-  () => import('./churvox-fresh/optionFXeroActionsRuntime'),
-  () => import('./churvox-fresh/optionFXeroPaymentsRuntime'),
-  () => import('./churvox-fresh/optionFOperationsRuntime'),
-  () => import('./churvox-fresh/optionFAdminBrainRuntime'),
-  () => import('./churvox-fresh/optionFNoLeakRuntime'),
-  () => import('./churvox-fresh/optionFHideSeedDemoRuntime'),
-  () => import('./churvox-fresh/optionFPlainLanguageRuntime'),
-  () => import('./churvox-fresh/optionFWorkerCopyScrubRuntime'),
-  () => import('./churvox-fresh/optionFHideHelperButtonsRuntime'),
-  () => import('./churvox-fresh/optionFCommandCleanupRuntime'),
-  () => import('./churvox-fresh/optionFTodayScheduleGuardRuntime'),
-  () => import('./churvox-fresh/optionFAutoAdminFlowRuntime'),
-  () => import('./churvox-fresh/optionFDecisionEffectsRuntime'),
-  () => import('./churvox-fresh/optionFRecordHydrationRuntime'),
-  () => import('./churvox-fresh/optionFAiFillMissingRuntime'),
-  () => import('./churvox-fresh/optionFDrawerPersistenceRuntime'),
-  () => import('./churvox-fresh/optionFReadinessActionFixRuntime'),
-  () => import('./churvox-fresh/optionFApprovalExecutionRuntime'),
-  () => import('./runtime/churvoxPaidLaunchOverflowRuntime'),
-  () => import('./runtime/churvoxOwnerVisualStabilityRuntime'),
-  () => import('./runtime/churvoxOwnerRecoverySlipRuntime'),
-  () => import('./runtime/churvoxOwnerRecoveryActionSlipRuntime'),
-  () => import('./runtime/churvoxOwnerPageContainmentRuntime'),
-  () => import('./runtime/churvoxOwnerProperPageLayoutsRuntime'),
-  () => import('./runtime/churvoxOwnerProperPageShieldRuntime'),
-  () => import('./runtime/churvoxOwnerProperFormsRuntime'),
-  () => import('./runtime/churvoxOwnerProperFormsTopRuntime'),
-  () => import('./runtime/churvoxProductPremiumVisualRuntime'),
-  () => import('./runtime/churvoxCenterSlipsRuntime'),
-  () => import('./runtime/churvoxOwnerButtonWiringRuntime'),
 ];
 
 const workerRuntimeImports = [
@@ -134,4 +80,3 @@ window.addEventListener('hashchange', checkRuntimeLoads);
 window.addEventListener('churvox-owner-app-ready', checkRuntimeLoads);
 setTimeout(checkRuntimeLoads, 300);
 setTimeout(checkRuntimeLoads, 800);
-setTimeout(checkRuntimeLoads, 1800);
