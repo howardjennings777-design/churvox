8b70a34ea8dbdce16b481efc97dec919303c82f6
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 15:45:51 +1200

    Show admin ledger in top nav

--- frontend/src/churvox-product/ProductAppV2WithLedger.jsx
+++ frontend/src/churvox-product/ProductAppV2WithLedger.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductAppV2 from "./ProductAppV2";
 import WorldAdminLedger from "./WorldAdminLedger";
+import WorldAdminLedgerNavBridge from "./WorldAdminLedgerNavBridge";
 
 export default function ProductAppV2WithLedger() {
   return <>
     <ProductAppV2 />
     <WorldAdminLedger />
+    <WorldAdminLedgerNavBridge />
   </>;
 }
