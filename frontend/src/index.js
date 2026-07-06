a58659b568c1de01f68fd4f6b50761237ffbc3a3
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 16:30:25 +1200

    Temporarily disable floating Smart Actions runtime

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,8 +31,6 @@ const ownerRuntimeImports = [
   () => import('./runtime/churvoxPlanPersistenceRuntime'),
   () => import('./runtime/churvoxCommandBrainRuntime'),
   () => import('./runtime/churvoxPaymentSetupRuntime'),
-  () => import('./runtime/churvoxSmartActionsRuntime'),
-  () => import('./runtime/churvoxSmartActionsCommandGuardRuntime'),
 ];
 
 const workerRuntimeImports = [
