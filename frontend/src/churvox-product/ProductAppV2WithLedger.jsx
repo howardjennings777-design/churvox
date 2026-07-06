2406a7a4e6c95fdae8e0f855a86c35970ccf92d1
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 15:37:05 +1200

    Wrap product app with world admin ledger

---
+++ frontend/src/churvox-product/ProductAppV2WithLedger.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductAppV2 from "./ProductAppV2";
+import WorldAdminLedger from "./WorldAdminLedger";
+
+export default function ProductAppV2WithLedger() {
+  return <>
+    <ProductAppV2 />
+    <WorldAdminLedger />
+  </>;
+}
