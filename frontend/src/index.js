f165e7d0ae656addacfd9d76ed141f0b5863ccd6
howardjennings777-design <howardjennings777@gmail.com>
2026-07-06 16:22:13 +1200

    Disable worker smart profile runtime after freeze report

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,7 +31,6 @@ const ownerRuntimeImports = [
   () => import('./runtime/churvoxPlanPersistenceRuntime'),
   () => import('./runtime/churvoxCommandBrainRuntime'),
   () => import('./runtime/churvoxPaymentSetupRuntime'),
-  () => import('./runtime/churvoxWorkerSmartProfileRuntime'),
   () => import('./runtime/churvoxSmartActionsRuntime'),
   () => import('./runtime/churvoxSmartActionsCommandGuardRuntime'),
 ];
