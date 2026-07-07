8cce2fc3d01a714c657dc296e3f840f3164ba783
howardjennings777-design <howardjennings777@gmail.com>
2026-07-08 10:56:50 +1200

    Remove floating admin ledger from owner app

--- frontend/src/churvox-product/ProductAppV2WithLedger.jsx
+++ frontend/src/churvox-product/ProductAppV2WithLedger.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ProductAppV2 from "./ProductAppV2";
-import WorldAdminLedger from "./WorldAdminLedger";
-import WorldAdminLedgerNavBridge from "./WorldAdminLedgerNavBridge";
 import { useApi } from "../hooks/useApi";
 
 function LeastAdminAutoScan() {
@@ -70,7 +68,5 @@ export default function ProductAppV2WithLedger() {
   return <>
     <LeastAdminAutoScan />
     <ProductAppV2 />
-    <WorldAdminLedger />
-    <WorldAdminLedgerNavBridge />
   </>;
 }
