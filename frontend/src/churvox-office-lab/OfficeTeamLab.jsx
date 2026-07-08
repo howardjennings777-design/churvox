f0fd3bec5737faaaba7961c3ea2b41fa32e534d3
howardjennings777-design <howardjennings777@gmail.com>
2026-07-09 10:58:43 +1200

    Install Office Team scroll guard

--- frontend/src/churvox-office-lab/OfficeTeamLab.jsx
+++ frontend/src/churvox-office-lab/OfficeTeamLab.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import OfficeTeamLabTidy from './OfficeTeamLabTidy'
+import OfficeTeamScrollGuard from './OfficeTeamScrollGuard'
 
 function OfficeTeamLab() {
-  return React.createElement(OfficeTeamLabTidy)
+  return React.createElement(React.Fragment, null,
+    React.createElement(OfficeTeamScrollGuard),
+    React.createElement(OfficeTeamLabTidy)
+  )
 }
 
 export default OfficeTeamLab
