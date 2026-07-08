0bd5f0d62cf6d32587a6c8fc69d7a4359f70f774
howardjennings777-design <howardjennings777@gmail.com>
2026-07-08 21:41:50 +1200

    Load on-site payments request signature fix

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -71,6 +71,7 @@ for _patch in [
     'churvox_hq_tester_status_patch',
     'churvox_hq_unique_visitors_patch',
     'churvox_on_site_payments_patch',
+    'churvox_on_site_payments_request_signature_fix_patch',
     'churvox_terminal_reader_patch',
     'churvox_os_v2_saved_records_patch',
     'churvox_owner_record_engine_routes_patch',
