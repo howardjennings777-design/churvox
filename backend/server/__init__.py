55810a4434804e7bf8fcc1c8d041763ad5730ef1
howardjennings777-design <howardjennings777@gmail.com>
2026-07-08 18:45:52 +1200

    Load business system suite in Render server wrapper

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -80,6 +80,8 @@ for _patch in [
     'churvox_api_request_422_fix_patch',
     'churvox_tester_email_send_final_patch',
     'churvox_email_provider_status_patch',
+    'churvox_business_logic_health_patch',
+    'churvox_business_system_suite_patch',
 ]:
     _install_launch_patch(_patch)
 
