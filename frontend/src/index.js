0a1ea27828f677f5f1799beca3accf958a1a2c80
howardjennings777-design <howardjennings777@gmail.com>
2026-07-09 06:06:39 +1200

    Load polished Messages page runtime

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -78,6 +78,7 @@ const ownerFastRuntimeImports = [
   () => import('./runtime/churvoxNavBadgesRuntime'),
   () => import('./runtime/churvoxVisibleLogoutRuntime'),
   () => import('./runtime/churvoxDrawerRecordActionsRuntime'),
+  () => import('./runtime/churvoxMessagesPagePolishRuntime'),
   () => import('./runtime/churvoxAppIndustryRuntime'),
   () => import('./runtime/churvoxInternalSupportRuntime'),
   () => import('./runtime/churvoxTidyRealSlipRuntime'),
