e77f657b7bcf9782794d97fe515e8396adafaaca
howardjennings777-design <howardjennings777@gmail.com>
2026-07-09 14:57:30 +1200

    Pass owner mode into communication screens

--- frontend/src/churvox-office-lab/OfficeTeamCommunicationScreens.jsx
+++ frontend/src/churvox-office-lab/OfficeTeamCommunicationScreens.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import OfficeTeamMessagesDesk from "./OfficeTeamMessagesDesk";
 import OfficeTeamWorkerPhoneView from "./OfficeTeamWorkerPhoneView";
 
-export function MessagesScreen() {
-  return <OfficeTeamMessagesDesk />;
+export function MessagesScreen(props) {
+  return <OfficeTeamMessagesDesk {...props} />;
 }
 
-export function WorkerViewScreen() {
-  return <OfficeTeamWorkerPhoneView />;
+export function WorkerViewScreen(props) {
+  return <OfficeTeamWorkerPhoneView {...props} />;
 }
