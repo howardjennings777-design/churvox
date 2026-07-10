e0d22ad4747ee94dec6000525a0c2529e4b2f0ae
howardjennings777-design <howardjennings777@gmail.com>
2026-07-11 00:14:57 +1200

    Load mobile public touch targets

--- frontend/src/pages/marketing/ChurvoxPublicShell.jsx
+++ frontend/src/pages/marketing/ChurvoxPublicShell.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ChurvoxLogo } from "../../components/ChurvoxLogo";
 import "./ChurvoxPublic2026.css";
+import "./ChurvoxPublicTouchTargets.css";
 
 const navItems = [
   ["/product", "Product"],
