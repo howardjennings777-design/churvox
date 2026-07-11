4ee01906a66e8b86b6a9e97edfdba6313539830d
howardjennings777-design <howardjennings777@gmail.com>
2026-07-12 10:43:07 +1200

    Protect legacy platform HQ page

--- frontend/src/pages/AppOwnerPage.jsx
+++ frontend/src/pages/AppOwnerPage.jsx
@@ -1,1 +1,11 @@
-export { default } from "./AppOwnerMachine";
+import React from "react";
+import PlatformAdminRoute from "../components/admin/PlatformAdminRoute";
+import AppOwnerMachine from "./AppOwnerMachine";
+
+export default function AppOwnerPage() {
+  return (
+    <PlatformAdminRoute>
+      <AppOwnerMachine />
+    </PlatformAdminRoute>
+  );
+}
