df47ef5925d00399ac88fe00c81808cda6ed945f
howardjennings777-design <howardjennings777@gmail.com>
2026-07-12 20:58:44 +1200

    Load the Product plans usage bridge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -89,6 +89,7 @@ const ownerFastRuntimeImports = [
   () => import('./runtime/churvoxOwnerHeaderLogoRuntime'),
   () => import('./runtime/churvoxMoreMenuPaidLaunchRuntime'),
   () => import('./runtime/churvoxPlansUsageTruthRuntime'),
+  () => import('./runtime/churvoxProductPlansUsageBridgeRuntime'),
   () => import('./runtime/churvoxBillingPortalRuntime'),
 ];
 
