c4cb985d500df807cde97776798c6853093874de
howardjennings777-design <howardjennings777@gmail.com>
2026-07-12 12:03:39 +1200

    Load final HQ control access route

--- backend/churvox_startup_patch_loader.py
+++ backend/churvox_startup_patch_loader.py
@@ -75,6 +75,7 @@ PATCH_MODULES = [
     "churvox_hq_tester_status_patch",
     "churvox_tester_email_case_preserve_patch",
     "churvox_hq_tester_system_patch",
+    "churvox_hq_control_access_final_patch",
     "churvox_business_logic_health_patch",
     "churvox_business_system_suite_patch",
     "churvox_hq_hello_canonical_patch",
