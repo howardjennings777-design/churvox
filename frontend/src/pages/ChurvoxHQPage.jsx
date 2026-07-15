dfda4c49dad7d77a60658700efa2fd1a52036207
howardjennings777-design <howardjennings777@gmail.com>
2026-07-15 13:14:31 +1200

    Add a durable HQ system boundary

--- frontend/src/pages/ChurvoxHQPage.jsx
+++ frontend/src/pages/ChurvoxHQPage.jsx
@@ -1,1 +1,14 @@
-export { default } from "./PaidLaunchHQSystem";
+import React from "react";
+import PaidLaunchHQSystem from "./PaidLaunchHQSystem";
+
+export default function ChurvoxHQPage() {
+  return (
+    <div
+      id="CHURVOX_HQ_SYSTEM"
+      data-version="CHURVOX_HQ_SYSTEM_FULL_SITE_AUDIT_20260715"
+      aria-label="Churvox HQ system"
+    >
+      <PaidLaunchHQSystem />
+    </div>
+  );
+}
