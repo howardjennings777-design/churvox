572004e49b0b34b30a28716c6198253503b31959
howardjennings777-design <howardjennings777@gmail.com>
2026-07-23 10:29:52 +1200

    Show stored proof beside the complete Command desk

--- frontend/src/churvox-office-os/OfficeOSWorkingConnected.jsx
+++ frontend/src/churvox-office-os/OfficeOSWorkingConnected.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import OfficeOSConnected from "./OfficeOSConnected";
 import OfficeOSQuickPrepare from "./OfficeOSQuickPrepare";
 import OfficeOSApprovalDesk from "./OfficeOSApprovalDesk";
+import OfficeOSPreparedRecords from "./OfficeOSPreparedRecords";
 
-export const OFFICE_OS_WORKING_CONNECTED_BUILD = "churvox-office-os-working-connected-20260723-complete-command-drafts";
+export const OFFICE_OS_WORKING_CONNECTED_BUILD = "churvox-office-os-working-connected-20260723-complete-command-proof";
 
 if (typeof window !== "undefined") {
   window.__CHURVOX_OFFICE_OS_WORKING_CONNECTED_BUILD__ = OFFICE_OS_WORKING_CONNECTED_BUILD;
@@ -15,6 +16,7 @@ export default function OfficeOSWorkingConnected() {
       <OfficeOSConnected />
       <OfficeOSQuickPrepare />
       <OfficeOSApprovalDesk />
+      <OfficeOSPreparedRecords />
     </div>
   );
 }
