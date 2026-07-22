06a313e28b82575fe70d543bb734fbd97c245d29
howardjennings777-design <howardjennings777@gmail.com>
2026-07-23 09:46:49 +1200

    Wire client approval desk into connected Office OS

--- frontend/src/churvox-office-os/OfficeOSWorkingConnected.jsx
+++ frontend/src/churvox-office-os/OfficeOSWorkingConnected.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import OfficeOSConnected from "./OfficeOSConnected";
 import OfficeOSQuickPrepare from "./OfficeOSQuickPrepare";
+import OfficeOSClientApprovalDesk from "./OfficeOSClientApprovalDesk";
 
-export const OFFICE_OS_WORKING_CONNECTED_BUILD = "churvox-office-os-working-connected-20260723";
+export const OFFICE_OS_WORKING_CONNECTED_BUILD = "churvox-office-os-working-connected-20260723-client-approval";
 
 if (typeof window !== "undefined") {
   window.__CHURVOX_OFFICE_OS_WORKING_CONNECTED_BUILD__ = OFFICE_OS_WORKING_CONNECTED_BUILD;
@@ -13,6 +14,7 @@ export default function OfficeOSWorkingConnected() {
     <div data-working-connected-office-os="true">
       <OfficeOSConnected />
       <OfficeOSQuickPrepare />
+      <OfficeOSClientApprovalDesk />
     </div>
   );
 }
