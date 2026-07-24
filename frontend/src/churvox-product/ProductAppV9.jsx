9794b0ea18855c737604bfb6e7de1de022d24c00
howardjennings777-design <howardjennings777@gmail.com>
2026-07-25 10:33:53 +1200

    Add Churvox Control Room V9 shell

---
+++ frontend/src/churvox-product/ProductAppV9.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductAppV8 from "./ProductAppV8";
+import "./productAppV9.css";
+
+export default function ProductAppV9() {
+  return (
+    <div className="cv9ControlRoom" data-churvox-layout="control-room-v9">
+      <ProductAppV8 />
+    </div>
+  );
+}
