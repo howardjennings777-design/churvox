0a7ba108aec4e8ae6138fadebc40c3020899f82f
howardjennings777-design <howardjennings777@gmail.com>
2026-07-25 13:12:56 +1200

    Apply fresh studio polish layer

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,4 +1,5 @@
 import "../churvox-studio/studioIconBridge";
 import ChurvoxStudioApp from "../churvox-studio/ChurvoxStudioApp";
+import "../churvox-studio/studioPolish.css";
 
 export default ChurvoxStudioApp;
