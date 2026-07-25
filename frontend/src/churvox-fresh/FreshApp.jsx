732ecc6b86b978b6fe6420b10ec92070294e5393
howardjennings777-design <howardjennings777@gmail.com>
2026-07-25 13:43:52 +1200

    Wire release bridge into the fresh owner app

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,5 +1,8 @@
 import "../churvox-studio/studioIconBridge";
 import ChurvoxStudioApp from "../churvox-studio/ChurvoxStudioApp";
+import StudioReleaseBridge from "../churvox-studio/StudioReleaseBridge";
 import "../churvox-studio/studioPolish.css";
 
-export default ChurvoxStudioApp;
+export default function FreshApp() {
+  return <><ChurvoxStudioApp /><StudioReleaseBridge /></>;
+}
