f0731b565ae6debf9ec19b225726888ddf7890fa
howardjennings777-design <howardjennings777@gmail.com>
2026-07-26 13:36:59 +1200

    Wire owner app cleanup pass

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -1,8 +1,10 @@
 import "../churvox-studio/studioIconBridge";
 import ChurvoxStudioApp from "../churvox-studio/ChurvoxStudioApp";
 import StudioReleaseBridge from "../churvox-studio/StudioReleaseBridge";
+import StudioCleanupBridge from "../churvox-studio/StudioCleanupBridge";
 import "../churvox-studio/studioPolish.css";
+import "../churvox-studio/studioCleanup.css";
 
 export default function FreshApp() {
-  return <><ChurvoxStudioApp /><StudioReleaseBridge /></>;
+  return <><ChurvoxStudioApp /><StudioReleaseBridge /><StudioCleanupBridge /></>;
 }
