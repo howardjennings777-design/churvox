df3909c8e3ac4d696f543db44ab71dadf93dbd64
howardjennings777-design <howardjennings777@gmail.com>
2026-07-27 07:22:50 +1200

    Load the modern Plans workspace styling

--- frontend/src/churvox-fresh/FreshApp.jsx
+++ frontend/src/churvox-fresh/FreshApp.jsx
@@ -4,6 +4,7 @@ import StudioReleaseBridge from "../churvox-studio/StudioReleaseBridge";
 import StudioCleanupBridge from "../churvox-studio/StudioCleanupBridge";
 import "../churvox-studio/studioPolish.css";
 import "../churvox-studio/studioCleanup.css";
+import "../churvox-studio/plansModern.css";
 
 export default function FreshApp() {
   return <><ChurvoxStudioApp /><StudioReleaseBridge /><StudioCleanupBridge /></>;
